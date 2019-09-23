f14708c45b9d927b1533a653f343b790b1a4e384
Administrator <1020064691@qq.com>
2019-09-23 17:42:44 +0800

    add cartView

---
+++ src/main/java/com/atguigu/bean/MlbackAddCartViewDetail.java
@@ -0,0 +1,103 @@
+package com.atguigu.bean;
+
+public class MlbackAddCartViewDetail {
+    private Integer addcartviewdetailId;
+
+    private String addcartviewdetailSessionid;
+
+    private String addcartviewdetailName;
+
+    private Integer addcartviewdetailProid;
+
+    private String addcartviewdetailProname;
+
+    private String addcartviewdetailSeoname;
+
+    private String addcartviewdetailCreatetime;
+
+    private String addcartviewdetailMotifytime;
+
+    private String addcartviewdetailStarttime;
+
+    private String addcartviewdetailEndtime;
+
+    public Integer getAddcartviewdetailId() {
+        return addcartviewdetailId;
+    }
+
+    public void setAddcartviewdetailId(Integer addcartviewdetailId) {
+        this.addcartviewdetailId = addcartviewdetailId;
+    }
+
+    public String getAddcartviewdetailSessionid() {
+        return addcartviewdetailSessionid;
+    }
+
+    public void setAddcartviewdetailSessionid(String addcartviewdetailSessionid) {
+        this.addcartviewdetailSessionid = addcartviewdetailSessionid == null ? null : addcartviewdetailSessionid.trim();
+    }
+
+    public String getAddcartviewdetailName() {
+        return addcartviewdetailName;
+    }
+
+    public void setAddcartviewdetailName(String addcartviewdetailName) {
+        this.addcartviewdetailName = addcartviewdetailName == null ? null : addcartviewdetailName.trim();
+    }
+
+    public Integer getAddcartviewdetailProid() {
+        return addcartviewdetailProid;
+    }
+
+    public void setAddcartviewdetailProid(Integer addcartviewdetailProid) {
+        this.addcartviewdetailProid = addcartviewdetailProid;
+    }
+
+    public String getAddcartviewdetailProname() {
+        return addcartviewdetailProname;
+    }
+
+    public void setAddcartviewdetailProname(String addcartviewdetailProname) {
+        this.addcartviewdetailProname = addcartviewdetailProname == null ? null : addcartviewdetailProname.trim();
+    }
+
+    public String getAddcartviewdetailSeoname() {
+        return addcartviewdetailSeoname;
+    }
+
+    public void setAddcartviewdetailSeoname(String addcartviewdetailSeoname) {
+        this.addcartviewdetailSeoname = addcartviewdetailSeoname == null ? null : addcartviewdetailSeoname.trim();
+    }
+
+    public String getAddcartviewdetailCreatetime() {
+        return addcartviewdetailCreatetime;
+    }
+
+    public void setAddcartviewdetailCreatetime(String addcartviewdetailCreatetime) {
+        this.addcartviewdetailCreatetime = addcartviewdetailCreatetime == null ? null : addcartviewdetailCreatetime.trim();
+    }
+
+    public String getAddcartviewdetailMotifytime() {
+        return addcartviewdetailMotifytime;
+    }
+
+    public void setAddcartviewdetailMotifytime(String addcartviewdetailMotifytime) {
+        this.addcartviewdetailMotifytime = addcartviewdetailMotifytime == null ? null : addcartviewdetailMotifytime.trim();
+    }
+
+    public String getAddcartviewdetailStarttime() {
+        return addcartviewdetailStarttime;
+    }
+
+    public void setAddcartviewdetailStarttime(String addcartviewdetailStarttime) {
+        this.addcartviewdetailStarttime = addcartviewdetailStarttime == null ? null : addcartviewdetailStarttime.trim();
+    }
+
+    public String getAddcartviewdetailEndtime() {
+        return addcartviewdetailEndtime;
+    }
+
+    public void setAddcartviewdetailEndtime(String addcartviewdetailEndtime) {
+        this.addcartviewdetailEndtime = addcartviewdetailEndtime == null ? null : addcartviewdetailEndtime.trim();
+    }
+}
