85e467d6c546386a1315e451438e7aafa5f924b0
Administrator <1020064691@qq.com>
2019-11-16 09:33:01 +0800

    addpayinfo,dao，mapper，service

---
+++ src/main/java/com/atguigu/bean/MlbackAddPayinfoViewDetail.java
@@ -0,0 +1,113 @@
+package com.atguigu.bean;
+
+public class MlbackAddPayinfoViewDetail {
+    private Integer addpayinfoviewdetailId;
+
+    private String addpayinfoviewdetailSessionid;
+
+    private String addpayinfoviewdetailPayinfoid;
+
+    private String addpayinfoviewdetailPayinfoshipnum;
+
+    private String addpayinfoviewdetailPayinfomoney;
+
+    private Integer addpayinfoviewdetailPayinfoaddressid;
+
+    private String addpayinfoviewdetailPayinfoaddressidname;
+
+    private String addpayinfoviewdetailCreatetime;
+
+    private String addpayinfoviewdetailMotifytime;
+
+    private String addpayinfoviewdetailStarttime;
+
+    private String addpayinfoviewdetailEndtime;
+
+    public Integer getAddpayinfoviewdetailId() {
+        return addpayinfoviewdetailId;
+    }
+
+    public void setAddpayinfoviewdetailId(Integer addpayinfoviewdetailId) {
+        this.addpayinfoviewdetailId = addpayinfoviewdetailId;
+    }
+
+    public String getAddpayinfoviewdetailSessionid() {
+        return addpayinfoviewdetailSessionid;
+    }
+
+    public void setAddpayinfoviewdetailSessionid(String addpayinfoviewdetailSessionid) {
+        this.addpayinfoviewdetailSessionid = addpayinfoviewdetailSessionid == null ? null : addpayinfoviewdetailSessionid.trim();
+    }
+
+    public String getAddpayinfoviewdetailPayinfoid() {
+        return addpayinfoviewdetailPayinfoid;
+    }
+
+    public void setAddpayinfoviewdetailPayinfoid(String addpayinfoviewdetailPayinfoid) {
+        this.addpayinfoviewdetailPayinfoid = addpayinfoviewdetailPayinfoid == null ? null : addpayinfoviewdetailPayinfoid.trim();
+    }
+
+    public String getAddpayinfoviewdetailPayinfoshipnum() {
+        return addpayinfoviewdetailPayinfoshipnum;
+    }
+
+    public void setAddpayinfoviewdetailPayinfoshipnum(String addpayinfoviewdetailPayinfoshipnum) {
+        this.addpayinfoviewdetailPayinfoshipnum = addpayinfoviewdetailPayinfoshipnum == null ? null : addpayinfoviewdetailPayinfoshipnum.trim();
+    }
+
+    public String getAddpayinfoviewdetailPayinfomoney() {
+        return addpayinfoviewdetailPayinfomoney;
+    }
+
+    public void setAddpayinfoviewdetailPayinfomoney(String addpayinfoviewdetailPayinfomoney) {
+        this.addpayinfoviewdetailPayinfomoney = addpayinfoviewdetailPayinfomoney == null ? null : addpayinfoviewdetailPayinfomoney.trim();
+    }
+
+    public Integer getAddpayinfoviewdetailPayinfoaddressid() {
+        return addpayinfoviewdetailPayinfoaddressid;
+    }
+
+    public void setAddpayinfoviewdetailPayinfoaddressid(Integer addpayinfoviewdetailPayinfoaddressid) {
+        this.addpayinfoviewdetailPayinfoaddressid = addpayinfoviewdetailPayinfoaddressid;
+    }
+
+    public String getAddpayinfoviewdetailPayinfoaddressidname() {
+        return addpayinfoviewdetailPayinfoaddressidname;
+    }
+
+    public void setAddpayinfoviewdetailPayinfoaddressidname(String addpayinfoviewdetailPayinfoaddressidname) {
+        this.addpayinfoviewdetailPayinfoaddressidname = addpayinfoviewdetailPayinfoaddressidname == null ? null : addpayinfoviewdetailPayinfoaddressidname.trim();
+    }
+
+    public String getAddpayinfoviewdetailCreatetime() {
+        return addpayinfoviewdetailCreatetime;
+    }
+
+    public void setAddpayinfoviewdetailCreatetime(String addpayinfoviewdetailCreatetime) {
+        this.addpayinfoviewdetailCreatetime = addpayinfoviewdetailCreatetime == null ? null : addpayinfoviewdetailCreatetime.trim();
+    }
+
+    public String getAddpayinfoviewdetailMotifytime() {
+        return addpayinfoviewdetailMotifytime;
+    }
+
+    public void setAddpayinfoviewdetailMotifytime(String addpayinfoviewdetailMotifytime) {
+        this.addpayinfoviewdetailMotifytime = addpayinfoviewdetailMotifytime == null ? null : addpayinfoviewdetailMotifytime.trim();
+    }
+
+    public String getAddpayinfoviewdetailStarttime() {
+        return addpayinfoviewdetailStarttime;
+    }
+
+    public void setAddpayinfoviewdetailStarttime(String addpayinfoviewdetailStarttime) {
+        this.addpayinfoviewdetailStarttime = addpayinfoviewdetailStarttime == null ? null : addpayinfoviewdetailStarttime.trim();
+    }
+
+    public String getAddpayinfoviewdetailEndtime() {
+        return addpayinfoviewdetailEndtime;
+    }
+
+    public void setAddpayinfoviewdetailEndtime(String addpayinfoviewdetailEndtime) {
+        this.addpayinfoviewdetailEndtime = addpayinfoviewdetailEndtime == null ? null : addpayinfoviewdetailEndtime.trim();
+    }
+}
