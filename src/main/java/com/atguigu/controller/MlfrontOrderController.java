ea05e38dd5b4ac32d6276461d4005b4eae3a73ed
Administrator <Administrator@SKY-20191211THQ>
2020-03-11 16:32:08 +0800

    shop by look,时间倒序

--- src/main/java/com/atguigu/controller/MlfrontOrderController.java
+++ src/main/java/com/atguigu/controller/MlfrontOrderController.java
@@ -49,7 +49,7 @@ import com.atguigu.service.MlfrontPayInfoService;
 import com.atguigu.utils.DateUtil;
 import com.atguigu.utils.EmailUtilshtml;
 import com.atguigu.utils.EmailUtilshtmlCustomer;
-import com.atguigu.utils.app.shipInformation;
+//import com.atguigu.utils.app.shipInformation;
 
 @Controller
 @RequestMapping("/MlfrontOrder")
@@ -678,16 +678,16 @@ public class MlfrontOrderController {
 		Integer addressId = mlfrontOrderRes.getAddressinfoId();
 		
 		//10.1向afterShip官方发送物流添加按钮
-		try {
-			//向物流中插入物流单号，订单号，Item,价格，
-			String resultStr =  shipInformation.addTrackingNumberIntoAfterShip(orderLogisticsnumber,payinfoPlateNum);
-			
-			System.out.println(resultStr);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("有异常");
-			System.out.println(e.getMessage());
-		}
+//		try {
+//			//向物流中插入物流单号，订单号，Item,价格，
+//			String resultStr =  shipInformation.addTrackingNumberIntoAfterShip(orderLogisticsnumber,payinfoPlateNum);
+//			
+//			System.out.println(resultStr);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			System.out.println("有异常");
+//			System.out.println(e.getMessage());
+//		}
 		
 		//10.2
 		sendLogisticsnumberEmail(addressId,orderLogisticsname,orderLogisticsnumber,orderId,payInfoId);
