30b13e097aa1cc48f714f276319dcec6e2642038
Administrator <Administrator@SKY-20191211THQ>
2020-03-21 11:46:03 +0800

    update

--- src/main/java/com/atguigu/controller/MlfrontOrderController.java
+++ src/main/java/com/atguigu/controller/MlfrontOrderController.java
@@ -49,7 +49,7 @@ import com.atguigu.service.MlfrontPayInfoService;
 import com.atguigu.utils.DateUtil;
 import com.atguigu.utils.EmailUtilshtml;
 import com.atguigu.utils.EmailUtilshtmlCustomer;
-//import com.atguigu.utils.app.shipInformation;
+import com.atguigu.utils.app.shipInformation;
 
 @Controller
 @RequestMapping("/MlfrontOrder")
@@ -780,16 +780,15 @@ public class MlfrontOrderController {
 		Integer addressId = mlfrontOrderRes.getAddressinfoId();
 		
 		//10.1向afterShip官方发送物流添加按钮
-//		try {
-//			//向物流中插入物流单号，订单号，Item,价格，
-//			String resultStr =  shipInformation.addTrackingNumberIntoAfterShip(orderLogisticsnumber,payinfoPlateNum);
-//			
-//			System.out.println(resultStr);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			System.out.println("有异常");
-//			System.out.println(e.getMessage());
-//		}
+		try {
+			//向物流中插入物流单号，订单号，Item,价格，
+			String resultStr =  shipInformation.addTrackingNumberIntoAfterShip(orderLogisticsnumber,payinfoPlateNum);
+			System.out.println(resultStr);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("有异常");
+			System.out.println(e.getMessage());
+		}
 		
 		//10.2
 		sendLogisticsnumberEmail(addressId,orderLogisticsname,orderLogisticsnumber,orderId,payInfoId);
@@ -1149,6 +1148,24 @@ public class MlfrontOrderController {
 		mlfrontPayInfoReq.setPayinfoStatus(6);//0未支付//1支付成功//2审单完毕//3发货完毕 //4已退款//5发送弃购//6重复单关闭
 		mlfrontPayInfoService.updateByPrimaryKeySelective(mlfrontPayInfoReq);
 
+		return Msg.success().add("Msg", "更新成功");
+	}
+	
+	/**
+	 * 14.0	UseNow	200309
+	 * to	订单-退款
+	 * @param jsp
+	 * @return 
+	 * */
+	@RequestMapping(value="/getCheckpointByTrackingNumber",method=RequestMethod.GET)
+	@ResponseBody
+	public Msg getCheckpointByTrackingNumber(HttpServletResponse rep,HttpServletRequest res,HttpSession session,
+			@RequestParam(value = "trackingNumber") String trackingNumber) {
+		//接收参数
+		
+		String trackingNumCheckpoint = shipInformation.getCheckpointByTrackingNumberFromAfterShip(trackingNumber);
+		System.out.println("trackingNumCheckpoint:"+trackingNumCheckpoint);
+		
 		return Msg.success().add("Msg", "更新成功");
 	}
 
